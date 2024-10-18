3c215ffb4f099f2595121409f4286211fb564ff5
vikash <120444720+vikashmurugesh@users.noreply.github.com>
2024-10-19 03:59:36 +0530

    setup.py
    i altered the existing code and solved some problmes

--- setup.py
+++ setup.py
@@ -10,13 +10,15 @@ def get_long_description(title):
     """Create the long_description from other files."""
     ROOT = os.path.abspath(os.path.dirname(__file__))
 
-    readme = open(os.path.join(ROOT, "README.rst"), "r", "utf8").read()
+    with open(os.path.join(ROOT, "README.rst"), "r", "utf8") as f:
+        readme = f.read()
     body_tag = ".. Omit badges from docs"
     readme_body_start = readme.index(body_tag)
     assert readme_body_start
-    readme_body = readme[readme_body_start + len(body_tag) :]
+    readme_body = readme[readme_body_start + len(body_tag):]
 
-    changelog = open(os.path.join(ROOT, "changelog.rst"), "r", "utf8").read()
+    with open(os.path.join(ROOT, "changelog.rst"), "r", "utf8") as f:
+        changelog = f.read()
     old_tag = ".. Omit older changes from package"
     changelog_body_end = changelog.index(old_tag)
     assert changelog_body_end
@@ -45,6 +47,7 @@ setup(
     setup_requires=["setuptools_scm"],
     description="Makes your Django tests simple and snappy",
     long_description=get_long_description("django-nose"),
+    long_description_content_type="text/x-rst",  # Specify the content type
     author="Jeff Balogh",
     author_email="me@jeffbalogh.org",
     maintainer="John Whitlock",
@@ -54,18 +57,11 @@ setup(
     packages=find_packages(exclude=["testapp", "testapp/*"]),
     include_package_data=True,
     zip_safe=False,
-    install_requires=["nose>=1.2.1"],
+    install_requires=[
+        "nose>=1.3.7",  # Updated to a more recent version of nose
+        "Django>=1.8,<3.0"  # Specify Django compatibility
+    ],
     test_suite="testapp.runtests.runtests",
-    # This blows up tox runs that install django-nose into a virtualenv,
-    # because it causes Nose to import django_nose.runner before the Django
-    # settings are initialized, leading to a mess of errors. There's no reason
-    # we need FixtureBundlingPlugin declared as an entrypoint anyway, since you
-    # need to be using django-nose to find the it useful, and django-nose knows
-    # about it intrinsically.
-    # entry_points="""
-    #    [nose.plugins.0.10]
-    #    fixture_bundler = django_nose.fixture_bundling:FixtureBundlingPlugin
-    #    """,
     keywords="django nose django-nose",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
